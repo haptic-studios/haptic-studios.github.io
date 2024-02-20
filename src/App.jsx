da6d79bc8b11e8659a1cdbd73962dca94505976f
Jarett Sisk <jarett.sisk@gmail.com>
2024-02-19 22:07:08 -0600

    initialized react project using vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+    <div style={{textAlign: "center", width: "100vw"}}>
+      <h1>Haptic Studios is currently under development.</h1> 
+      <p>Come see us again soon :)</p>      
+    </div>
+    </>
+  )
+}
+
+export default App

1ecb8f929901dff0966a3e969c411baa55cbe948
Jarett Sisk <jarett.sisk@gmail.com>
2024-02-19 22:39:37 -0600

    test push

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
     <div style={{textAlign: "center", width: "100vw"}}>
       <h1>Haptic Studios is currently under development.</h1> 
-      <p>Come see us again soon :)</p>      
+      <p>Come see us again soon! :)</p>      
     </div>
     </>
   )

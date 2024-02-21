2e1c0053386bc80a60878f53db5bedefebc66915
Hey_Tack <jsisk415@gmail.com>
2024-02-20 23:39:28 -0600

    Fixing url issues with static content

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 import { motion } from 'framer-motion';
+import videoUrl from '/src/videos/split-the-world-bg.mp4';
+import logoUrl from '/src/images/haptic-studios-inverted.png';
+
 
 export default function Home() {
     return (
@@ -9,9 +12,9 @@ export default function Home() {
       transition={{ duration: 0.5 }}
       exit={{opacity: 0}}
       className="content-layout">
-        <video className="logo-video" src={`/src/videos/split-the-world-bg.mp4#t=${Math.random() * 60}`} autoPlay loop muted/>
+        <video className="logo-video" src={`${videoUrl}#t=${Math.random() * 60}`} autoPlay loop muted/>
         <div className="logo-container">
-          <img className="studio-logo" src="/src/images/haptic-studios-inverted.png"/>
+          <img className="studio-logo" src={logoUrl}/>
           <Link to="/about" className="spaced-letters logo-link">THE TEAM</Link>
           <Link to="/games" className="spaced-letters logo-link">THE GAMES</Link>
         </div>

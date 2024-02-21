9eaa63a04827b4a06280995aea39c68ad39088f7
Hey_Tack <jsisk415@gmail.com>
2024-02-20 23:29:39 -0600

    Testing out transitions... worries they may not work, will need further testing

---
+++ src/components/no-match.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import { motion } from 'framer-motion';
+
+export default function NoMatch() {
+    return (
+      <motion.div 
+      initial={{ opacity: 0}}
+      animate={{ opacity: 1}}
+      transition={{ duration: 0.5 }}
+      exit={{opacity: 0}}
+      className="content-layout">
+      <h2>About</h2>
+        <h2>Nothing to see here!</h2>
+        <p>
+          <Link to="/">Go to the home page</Link>
+        </p>
+      </motion.div>
+    );
+  }

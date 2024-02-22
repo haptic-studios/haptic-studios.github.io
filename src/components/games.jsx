cd907e15b883cb8ef7663325ca5d0fdf1b50ecc3
Jarett Sisk <jarett.sisk@gmail.com>
2024-02-21 21:16:30 -0600

    some progress

--- src/components/games.jsx
+++ src/components/games.jsx
@@ -2,14 +2,10 @@ import { Link } from "react-router-dom";
 import { motion } from 'framer-motion';
 
 export default function Games() {
-    return (
-      <motion.div 
-      initial={{ opacity: 0}}
-      animate={{ opacity: 1}}
-      transition={{ duration: 0.5 }}
-      exit={{opacity: 0}}
-      className="content-layout">
+  usePageTitle("Haptic Studios - Games");
+  return (
+    <CrossfadeContainer>
       <h2>About</h2>
-      </motion.div>
-    )
-  }
+    </CrossfadeContainer>
+  )
+}
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,22 +2,23 @@ import { Link } from "react-router-dom";
 import { motion } from 'framer-motion';
 import videoUrl from '/src/videos/split-the-world-bg.mp4';
 import logoUrl from '/src/images/haptic-studios-inverted.png';
+import usePageTitle from "../helpers/customHooks/usePageTitle";
+import CrossfadeContainer from "./CrossfadeContainer";
 
 
 export default function Home() {
+    usePageTitle("Haptic Studios - Home");
     return (
-      <motion.div 
-      initial={{ opacity: 0}}
-      animate={{ opacity: 1}}
-      transition={{ duration: 0.5 }}
-      exit={{opacity: 0}}
-      className="content-layout">
-        <video className="logo-video" src={`${videoUrl}#t=${Math.random() * 60}`} autoPlay loop muted/>
+      <CrossfadeContainer>
+        <div className="logo-video-container">
+          <video className="logo-video" src={`${videoUrl}#t=${Math.random() * 60}`} autoPlay loop muted/>
+          <div class="overlay"></div>
+        </div>
         <div className="logo-container">
           <img className="studio-logo" src={logoUrl}/>
           <Link to="/about" className="spaced-letters logo-link">THE TEAM</Link>
           <Link to="/games" className="spaced-letters logo-link">THE GAMES</Link>
         </div>
-      </motion.div>
+      </CrossfadeContainer>
     )
   }

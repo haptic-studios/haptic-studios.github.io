cec1d4cd74cefe0e7ec0ccad67318d2d590f2734
Hey_Tack <jsisk415@gmail.com>
2024-02-23 16:10:31 -0600

    Addressing lint errors

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -11,7 +11,7 @@ export default function AboutPage() {
         <CrossfadeContainer>
           <div className="parallax-section" >
             <div className="overlay"></div>
-            <Image className="background" src={logoSVG} />
+            <Image className="background" src={logoSVG} alt="The Haptic Studios logo" />
             <div className="title-container">
               <h2 className="title spaced-letters">WHO ARE WE?</h2>
             </div>
@@ -20,23 +20,23 @@ export default function AboutPage() {
             <p>Haptic Studios is an independent game development studio, based in the United States. Founded in 2024, by brothers ILLUMINI and Hey_Tack.</p>
           </div>
           <div className="parallax-section justify-end">
-              <Image className="background" src={illuminiBgImage} />
+              <Image className="background" src={illuminiBgImage} alt="An image of ILLUMINI, leaning against a brick wall."/>
               <h2 className="title spaced-letters text-border">ILLUMINI</h2>
               <h2 className="title spaced-letters text-border">SOUND DESIGN | DEVELOPMENT | MARKETING</h2>
           </div>
           <div className="divider">
             <p>Sound Designer, Devleoper, and Marketing Director for Haptic Studios.</p>
-            <i>"Hey there, I'm Jarett Sisk, co-founder here at Haptic Studios.
-  Growing up, my twin brother and I always had a controller in hand, lost in the world of pixels. Starting a game studio was honestly a dream we both had since childhood. Fast forward, and here we are, steering the ship at Haptic Studios. It's not just a job; it's a crazy, exciting journey. I hope you enjoy playing our games as much as we enjoy creating them!"</i>
+            <i>&quot;Hey there, I&apos;m Jarett Sisk, co-founder here at Haptic Studios.
+  Growing up, my twin brother and I always had a controller in hand, lost in the world of pixels. Starting a game studio was honestly a dream we both had since childhood. Fast forward, and here we are, steering the ship at Haptic Studios. It&apos;s not just a job; it&apos;s a crazy, exciting journey. I hope you enjoy playing our games as much as we enjoy creating them!&quot;</i>
           </div>
           <div className="parallax-section">
-              <Image className="background-left" src={tackBgImage} />
+              <Image className="background-left" src={tackBgImage} alt="Hey_Tack, sitting at a computer in a dark room, wearing a blanket"/>
               <h2 className="title spaced-letters">HEY_TACK</h2>
               <h2 className="title spaced-letters">PROGRAMMING | ART | DESIGN</h2>
           </div>
           <div className="divider">
             <p>Lead Developer, Game Designer, and Artist for Haptic Studios.</p> 
-            <i>"The first game I ever played was Diddy Kong Racing on the N64 - and I distinctly remember asking myself `How do people make these?`. <br/> Here I am nearly 20 years later, and there are times where I feel no closer to answering that question."</i>
+            <i>&quot;The first game I ever played was Diddy Kong Racing on the N64 - and I distinctly remember asking myself `How do people make these?`. <br/> Here I am nearly 20 years later, and there are times where I feel no closer to answering that question.&quot;</i>
             <br/>
             <Link className='dark-link spaced-letters link-hover-effect' href="/">RETURN TO MAIN PAGE</Link>
           </div>

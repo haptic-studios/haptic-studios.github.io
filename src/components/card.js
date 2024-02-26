a6c24d344b393a0ca76e82b771d35952a6bc93d0
Hey_Tack <jsisk415@gmail.com>
2024-02-25 19:10:49 -0600

    First attempt at video on hover

--- src/components/card.js
+++ src/components/card.js
@@ -1,13 +1,31 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { useRef } from "react";
 
 const logoSVG = '/images/haptic-studios-compact-logo.svg';
+const videoUrl = "/videos/split-the-world-bg.mp4";
 
-export default function Card({cardImageSrc, cardImageAlt, title, shortDescription}) {
+function onMouseHover(event, videoRef) {
+    console.log(videoRef)
+    videoRef.current.play();
+}
+
+function onMouseExit(event, videoRef) {
+    console.log(videoRef)
+    videoRef.current.pause();
+}
+
+export default function Card({cardImageSrc, cardImageAlt, hoverVideo, title, shortDescription}) {
+    const videoRef = useRef(null);
     return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <div className="card max-w-sm rounded overflow-hidden shadow-lg"
+            onMouseOver={event => onMouseHover(event, videoRef)}
+            onMouseLeave={event => onMouseExit(event, videoRef)}>
             <div className="card-image-container">
                 <Image className="w-full" fill src={cardImageSrc || logoSVG} alt={cardImageAlt || "Splash screen for game"}/>
+                <video ref={videoRef} className="w-full hiddenVideo" src={hoverVideo || videoUrl} loop muted playsInline disablePictureInPicture/>
             </div>
             <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2">{title}</div>

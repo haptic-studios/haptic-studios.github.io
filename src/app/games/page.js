a11fc93eb1f5a55022a23aa10ef625dc701586c6
Hey_Tack <jsisk415@gmail.com>
2024-02-25 19:43:23 -0600

    Adding games, still need to setup links, platform information, and any other info we want to show on the cards.

--- src/app/games/page.js
+++ src/app/games/page.js
@@ -1,6 +1,12 @@
 import CrossfadeContainer from '@/helpers/CrossfadeContainer';
 import Card from '@/components/card';
 
+const splitTheWorldImage = './images/split-the-world.png';
+const splitTheWorldVideo = './videos/split-the-world-bg.mp4';
+
+const kingOfLiftImage = './images/king-of-lift.png';
+const kingOFLiftVideo = './videos/king-of-lift.mp4';
+
 export default function Games() {
     return (
         <CrossfadeContainer>
@@ -8,9 +14,23 @@ export default function Games() {
             <h1 className="spaced-letters">GAMES</h1>
           </div>
           <div className="game-list">
-            <Card title="Split The World" shortDescription="Wield the legendary sword, and cleave the world asunder."></Card>
-            <Card title="King of Lift" shortDescription="You've fallen in love with the pricess, do you have the strength to win the King's approval?"></Card>
-            <Card title="Test Title" shortDescription="This is not the game that you're looking for."></Card>
+            <Card 
+              title="Split The World" 
+              imageSrc={splitTheWorldImage}
+              imageAlt="The title screen for the game 'Split The World'"
+              hoverVideo={splitTheWorldVideo}
+              shortDescription="Wield the legendary sword, and cleave the world asunder."
+            ></Card>
+            <Card 
+              title="King of Lift" 
+              imageSrc={kingOfLiftImage}
+              imageAlt="The title image for the game 'King of Lift'"
+              hoverVideo={kingOFLiftVideo}
+              shortDescription="You've fallen in love with the pricess, do you have the strength to win the King's approval?"
+            ></Card>
+            <div className="stay-in-touch">
+              <i>And more to come...</i>
+            </div>
           </div>
         </CrossfadeContainer>
     );
--- src/components/card.js
+++ src/components/card.js
@@ -8,30 +8,28 @@ const logoSVG = '/images/haptic-studios-compact-logo.svg';
 const videoUrl = "/videos/split-the-world-bg.mp4";
 
 function onMouseHover(event, videoRef) {
-    console.log(videoRef)
     videoRef.current.play();
 }
 
 function onMouseExit(event, videoRef) {
-    console.log(videoRef)
     videoRef.current.pause();
 }
 
-export default function Card({cardImageSrc, cardImageAlt, hoverVideo, title, shortDescription}) {
+export default function Card({imageSrc, imageAlt, hoverVideo, title, shortDescription}) {
     const videoRef = useRef(null);
     return (
         <div className="card max-w-sm rounded overflow-hidden shadow-lg"
             onMouseOver={event => onMouseHover(event, videoRef)}
             onMouseLeave={event => onMouseExit(event, videoRef)}>
             <div className="card-image-container">
-                <Image className="w-full" fill src={cardImageSrc || logoSVG} alt={cardImageAlt || "Splash screen for game"}/>
+                <Image className="w-full" fill src={imageSrc || logoSVG} alt={imageAlt || "Splash screen for game"}/>
                 <video ref={videoRef} className="w-full hiddenVideo" src={hoverVideo || videoUrl} loop muted playsInline disablePictureInPicture/>
             </div>
             <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2">{title}</div>
-                <p className="text-gray-700 text-base">
+                <i className="text-base p-0 text-left">
                 {shortDescription}
-                </p>
+                </i>
             </div>
             <div className="px-6 pt-4 pb-2">
                 <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
